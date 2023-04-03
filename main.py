1c417a7ef93495daaadaf71410116276e708faab
Владислав <90913741+PuzyrevHarkovskii@users.noreply.github.com>
2023-04-03 19:15:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
 import sys
+import random
+from pygame.math import Vector2
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+        
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x * cell_size,self.pos.y * cell_size, cell_size, cell_size) 
+        pygame.draw.rect(screen, (126, 160, 114), fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100,200))
-test_surface.fill((0,0,255))
-x_pos = 200
+
+fruit = FRUIT()
+ 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
            pygame.quit()
            sys.exit()
-    screen.fill((175,215,70))    
-    x_pos += 1
-    screen.blit(test_surface,(x_pos,250))
+           
+    screen.fill((175,215,70))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
