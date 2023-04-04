011a92c96208d7bdd2a2c7c5617a5e476e5691cd
Владислав <90913741+PuzyrevHarkovskii@users.noreply.github.com>
2023-04-04 19:44:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pygame.math import Vector2
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
-        self.direction = Vector2(1,0)
+        self.direction = Vector2(0,0)
         self.new_block = False
         
         self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
@@ -94,6 +94,10 @@ class SNAKE:
     def play_crunch_sound(self):
         self.crunch_sound.play()
         
+    def reset(self):
+        self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
+        self.direction = Vector2(0,0)
+        
                 
 class MAIN:
     def __init__(self):
@@ -130,8 +134,7 @@ class MAIN:
                 self.game_over()
     
     def game_over(self):
-        pygame.quit()
-        sys.exit()
+        self.snake.reset()
         
     def draw_grass(self):
         grass_color = (167, 209, 61)
